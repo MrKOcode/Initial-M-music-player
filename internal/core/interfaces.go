d4a740bcd30f1f6c8ef7252212d38226820bfd40
Ray <ray_wei_@hotmail.com>
2025-10-01 17:48:04 -0700

    finished interface file for core

--- internal/core/interfaces.go
+++ internal/core/interfaces.go
@@ -15,3 +15,45 @@ type Decoder interface {
 	//Decode produces a decodedStream and basic matadata
 	Decoder(ctx context.Context, path string) (DecodedStream, Track, error)
 }
+
+
+//DecodedStream is intentionally opaque to the player
+//The output knows how to handle it
+type DecodedStream interface {
+	Close() error
+}
+
+//output plays a decoded stream
+type Output interface {
+	Play(stream DecodedStream, onEnd func()) 
+	Pause()
+	Resume()
+	Stop()
+	State() OutputState
+}
+
+type OutputState int
+
+const (
+	Stopped OutputState = iota
+	Playing
+	Paused
+)
+
+//playlist abstracts a list of tracks
+type Playlist interface {
+	Current() (Track, int, bool)
+	Next() (Track, int, bool)
+	Previous() (Track, int, bool)
+	JumpTo(i int) (Track, int, bool)
+	Len() int
+}
+
+//Registry for playable plugins
+type Registry interface {
+	RegisterDecoder(Decoder)	
+	FindDecoder(path string) Decoder
+}
+
+
+
